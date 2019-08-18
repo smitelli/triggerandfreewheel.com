21682ade8b599d611b4edc803d607d1a3fbfabbb
Scott Smitelli <scott@smitelli.com>
2019-08-18 10:44:23 -0400

    I do not care about Google Analytics anymore

--- public/static/js/scripts.js
+++ public/static/js/scripts.js
@@ -1,13 +1,5 @@
 var addthis_config = { 'data_track_clickback': true };
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-7354261-1'], ['_trackPageview']);
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 $(document).ready(function() {
   $('a[rel=external], a[rel=license]').attr('target', '_blank');
 
